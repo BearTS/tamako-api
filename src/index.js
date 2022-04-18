acb42fd74d2f04aa7cc874bb493ca7a9371ea71f
NotKaskus <andrewonrubia@gmail.com>
2022-04-18 09:45:30 +0800

    feat: Add ratelimiter, disable x-powered-by header, use xss protection, use cors, and add favicon

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
 const express = require('express');
 const path = require('path');
-// const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
+const cors = require('cors');
+const favicon = require('serve-favicon');
+
+// Global Vars
 require('dotenv').config();
 require('./resources/fonts');
-
-// eslint-disable-next-line no-unused-vars
-const { handleNotFound, handleInternalError } = require('./middleware/errorHandler');
 
 // Main App
 const app = express();
 
 // Middlewares
-app.set('trust proxy', 1);
-app.set('json spaces', 2);
+// eslint-disable-next-line no-unused-vars
+const { handleNotFound, handleInternalError } = require('./middleware/errorHandler');
+const rateLimiter = require('./middleware/rateLimiter');
 
 // Express App Configuration 
-
+app.disable('x-powered-by');
+app.use(xss());
+app.use(cors());
+app.set('trust proxy', 1);
+app.set('json spaces', 2);
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.engine('html', require('ejs').renderFile);
+
+app.use(favicon(path.resolve('public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, '../public/')));
 app.set('views', path.join(__dirname, '../public/views'));
-app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+// Ratelimiter
+app.use(rateLimiter);
 
 // Main route
 app.use('/', require('./routes/index'));
