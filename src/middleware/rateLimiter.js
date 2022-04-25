85aee8e697059d1377fcb3e4228be84a6d1258df
NotKaskus <andrewonrubia@gmail.com>
2022-04-25 09:25:31 +0800

    chore: make different ratelimiter for auth endpoint and normal endpoint

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -2,21 +2,27 @@ const { RateLimiterMemory } = require('rate-limiter-flexible');
 const { getClientIp } = require('request-ip');
 const moment = require('moment');
 const { handleRateLimit } = require('./errorHandler');
+// eslint-disable-next-line no-unused-vars
+const { userTable } = require('../database/main');
 
-// Rate limit options
-const opts = {
+// Rate limiter options
+const mainLimiter = new RateLimiterMemory({
     points: 90, // 90 requests
     duration: 60, // per 60 second by IP,
     blockDuration: 60 * 15 // Block for 15 minutes after reaching the limit
-};
+});
 
-const limiter = new RateLimiterMemory(opts);
+const authLimiter = new RateLimiterMemory({
+    points: 120, // 90 requests
+    duration: 60, // per 60 second by IP,
+    blockDuration: 60 * 15 // Block for 15 minutes after reaching the limit
+});
 
-const rateLimiter = async (req, res, next) => {
-    limiter.consume(getClientIp(req))
+const mainRateLimiter = async (req, res, next) => {
+    mainLimiter.consume(getClientIp(req))
         .then((rateLimiterRes) => {
             res.set({
-                'X-RateLimit-Limit': opts.points,
+                'X-RateLimit-Limit': mainLimiter.points,
                 'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
                 'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
             });
@@ -25,7 +31,7 @@ const rateLimiter = async (req, res, next) => {
         .catch((rateLimiterRes) => {
             res.set('Retry-After', moment.utc((rateLimiterRes.msBeforeNext / 1000) * 1000).format('mm:ss')); 
             res.set({
-                'X-RateLimit-Limit': opts.points,
+                'X-RateLimit-Limit': mainLimiter.points,
                 'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
                 'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
             });
@@ -33,4 +39,28 @@ const rateLimiter = async (req, res, next) => {
         });
 };
 
-module.exports = rateLimiter;
+const authRateLimiter = async (req, res, next) => {
+    authLimiter.consume(getClientIp(req))
+        .then((rateLimiterRes) => {
+            res.set({
+                'X-RateLimit-Limit': authLimiter.points,
+                'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
+                'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
+            });
+            next();
+        })
+        .catch((rateLimiterRes) => {
+            res.set('Retry-After', moment.utc((rateLimiterRes.msBeforeNext / 1000) * 1000).format('mm:ss')); 
+            res.set({
+                'X-RateLimit-Limit': authLimiter.points,
+                'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
+                'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
+            });
+            handleRateLimit(req, res); // Send an rate limited message
+        });
+};
+
+module.exports = {
+    mainRateLimiter,
+    authRateLimiter
+};
