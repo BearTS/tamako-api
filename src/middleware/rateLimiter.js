1099fa37dad4543624444cc7e7e98c78970b95e1
NotKaskus <andrewonrubia@gmail.com>
2022-04-25 09:46:34 +0800

    chore: merge two ratelimiter to one middleware

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -7,60 +7,53 @@ const { userTable } = require('../database/main');
 
 // Rate limiter options
 const mainLimiter = new RateLimiterMemory({
-    points: 90, // 90 requests
-    duration: 60, // per 60 second by IP,
-    blockDuration: 60 * 15 // Block for 15 minutes after reaching the limit
-});
-
-const authLimiter = new RateLimiterMemory({
     points: 120, // 90 requests
     duration: 60, // per 60 second by IP,
     blockDuration: 60 * 15 // Block for 15 minutes after reaching the limit
 });
 
 const mainRateLimiter = async (req, res, next) => {
-    mainLimiter.consume(getClientIp(req))
-        .then((rateLimiterRes) => {
-            res.set({
-                'X-RateLimit-Limit': mainLimiter.points,
-                'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
-                'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
+    if (req.path.indexOf('/api/v1') === 0) {
+        mainLimiter.consume(getClientIp(req))
+            .then((rateLimiterRes) => {
+                res.set({
+                    'X-RateLimit-Limit': mainLimiter.points,
+                    'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
+                    'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
+                });
+                next();
+            })
+            .catch((rateLimiterRes) => {
+                res.set('Retry-After', moment.utc((rateLimiterRes.msBeforeNext / 1000) * 1000).format('mm:ss')); 
+                res.set({
+                    'X-RateLimit-Limit': mainLimiter.points,
+                    'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
+                    'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
+                });
+                handleRateLimit(req, res); // Send an rate limited message
             });
-            next();
-        })
-        .catch((rateLimiterRes) => {
-            res.set('Retry-After', moment.utc((rateLimiterRes.msBeforeNext / 1000) * 1000).format('mm:ss')); 
-            res.set({
-                'X-RateLimit-Limit': mainLimiter.points,
-                'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
-                'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
+    } else {
+        mainLimiter.consume(getClientIp(req))
+            .then((rateLimiterRes) => {
+                res.set({
+                    'X-RateLimit-Limit': mainLimiter.points,
+                    'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
+                    'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
+                });
+                next();
+            })
+            .catch((rateLimiterRes) => {
+                res.set('Retry-After', moment.utc((rateLimiterRes.msBeforeNext / 1000) * 1000).format('mm:ss')); 
+                res.set({
+                    'X-RateLimit-Limit': mainLimiter.points,
+                    'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
+                    'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
+                });
+                handleRateLimit(req, res); // Send an rate limited message
             });
-            handleRateLimit(req, res); // Send an rate limited message
-        });
-};
-
-const authRateLimiter = async (req, res, next) => {
-    authLimiter.consume(getClientIp(req))
-        .then((rateLimiterRes) => {
-            res.set({
-                'X-RateLimit-Limit': authLimiter.points,
-                'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
-                'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
-            });
-            next();
-        })
-        .catch((rateLimiterRes) => {
-            res.set('Retry-After', moment.utc((rateLimiterRes.msBeforeNext / 1000) * 1000).format('mm:ss')); 
-            res.set({
-                'X-RateLimit-Limit': authLimiter.points,
-                'X-RateLimit-Remaining': rateLimiterRes.remainingPoints,
-                'X-RateLimit-Reset': new Date(Date.now() + rateLimiterRes.msBeforeNext)
-            });
-            handleRateLimit(req, res); // Send an rate limited message
-        });
+    }
 };
 
 module.exports = {
-    mainRateLimiter,
-    authRateLimiter
+    mainRateLimiter
 };
